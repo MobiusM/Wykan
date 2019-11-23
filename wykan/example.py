d8b068743d8be0923612bc8a98a8195183fe055c
MobiusM <danholandi@gmail.com>
2019-11-24 01:18:22 +0200

    Update garbage example.py

--- wykan/example.py
+++ wykan/example.py
@@ -8,14 +8,19 @@ if __name__ == '__main__':
     wekan = Wykan(wekan_url, username, password)
     print("Init wekan")
 
-    all_users = wekan.get_all_users()
-    print("got all users")
-    by_id = wekan.get_user("kPEmhfAFwJYwNRCp9")
-    print("got user by id")
-    by_username = wekan.get_user_by_username("levinson")
-    print("got user by username")
+    levinson = wekan.get_user_by_username("levinson")
+    # test_user = wekan.get_user_by_username("test_user")
+    print("got levinson by username")
+    # new_user = wekan.create_new_user("test_user", "test_user@schooll.dommm", "Aa12345")
+    # print("created new user")
+    # new_board = wekan.create_board("my new test board", test_user.id)
 
-    deleted_user_id = wekan.delete_user_by_username("bis-hanich")
-    # new_user = wekan.create_new_user("bis-hanich-test2", "hanich-mail@school.notreall", "Aa123456")
+    # test_user_boards = wekan.get_user_boards(test_user.id)
+    # target_board = test_user_boards[2]
 
+    # target_board.add_board_member(levinson.id, True, False, False)
+    # target_board.change_member_permissions(levinson.id, True, False, False)
+
+    levinson_boards = wekan.get_user_boards(levinson.id)
+    board_lists = levinson_boards[3].get_lists()
     print("breakpoint")
