5ad3e4fee5132e8c911df4d7daf0fe59f28c0100
MobiusM <danholandi@gmail.com>
2020-10-31 22:10:13 +0200

    Make Wykan a Python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Wykan',
+    version='1.0.0',
+    author='Dan Levinson',
+    author_email='danholandi@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/MobiusM/Wykan',
+    license='MIT',
+    description='Python wrapper for Wekan\'s REST API.'
+)
